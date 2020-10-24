74df9e7d8707861ca2ba2a1f95981f099e126374
kelseywhallon <kelsey.whallon@gmail.com>
2020-10-24 15:31:57 -0400

    lauren's updates on route & api

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ const flash = require('connect-flash');
 const passport = require('./config/ppConfig');
 const isLoggedIn = require('./middleware/isLoggedIn');
 const axios = require('axios')
-const apiKey = 'eef099ab373af0f9c3fc4b45d2dae0e6a058e317'
+const methodOverride = require('method-override')
+const apiKey = '34e71e65fea4bd5480247027ff3d653d33f1961f'
 const app = express();
+const db = require('./models')
 
 app.set('view engine', 'ejs');
 
@@ -26,6 +28,9 @@ app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 
+// use method override to handle PUT and DELETE requests elegantly
+app.use(methodOverride('_method'))
+
 
 app.use((req, res, next) => {
   // before every route, attach the flash messages and current user to res.locals
@@ -34,16 +39,58 @@ app.use((req, res, next) => {
   next();
 });
 
+// Homepage
 app.get('/', (req, res) => {
   res.render('index');
 });
 
+function makeGetRequest(path) { 
+  axios.get(path).then( 
+      (response) => { 
+          var result = response.data; 
+          console.log(result); 
+      }, 
+      (error) => { 
+          console.log(error); 
+      } 
+  ); 
+} 
+makeGetRequest('https://wger.de/api/v2/exercise/'); 
+
+// Individually chosen workouts
+app.get('/workout/:id', isLoggedIn, (req, res) => {
+  res.render('workout')
+})
+
+// Profile Page
 app.get('/profile', isLoggedIn, (req, res) => {
   res.render('profile');
 });
 
+app.get('/profile/:id', isLoggedIn, (req, res) => {
+  res.render('profile');
+});
+
+// Journal Entries
+app.get('/journal', isLoggedIn, (req, res) => {
+  res.render('journal')
+})
+
+app.post('/journal', isLoggedIn, (req, res) => {
+  res.render('journal')
+})
+
+app.delete('/journal', isLoggedIn, (req, res) => {
+  res.send('we want to delete something here')
+  // maybe: db.user_journal.destroy({
+  // where: {
+  // userId = '${user}
+  //   }
+  // })
+})
+
 app.use('/auth', require('./routes/auth'));
 
-var server = app.listen(process.env.PORT || 3000, ()=> console.log(`🎧You're listening to the smooth sounds of port ${process.env.PORT || 3000}🎧`));
+var server = app.listen(process.env.PORT || 3000, () => console.log(`🎧You're listening to the smooth sounds of port ${process.env.PORT || 3000}🎧`));
 
 module.exports = server;
